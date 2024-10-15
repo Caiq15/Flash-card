0cccc41a7818d261f6057aff8c60babad3c99829
Caiq15 <148580842+Caiq15@users.noreply.github.com>
2024-10-15 15:59:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Phython é uma linguagem de programação',
+)
+
+criaCartao(
+    'Geografia'
+    'Qual é a capital da França?',
+    'A capital da França é Paris',
+)
+
+criaCartao(
+    'Programação'
+    'O que é uma função'
+    'Uma função é um bloco de código que executa alguma tarefa',
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)',
+)
+
+criaCartao(
+    '',
+    '',
+    '',
+)
+
+criaCartao(
+    '',
+    '',
+    '',
+)
